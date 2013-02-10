bdbc1251602e45057927498cfe675761168be107
Rufus Pollock <rufus.pollock@okfn.org>
2013-02-10 22:43:28 +0000

    [#18,view][s]: add twitter share this button - fixes #18.

--- js/view.js
+++ js/view.js
@@ -69,7 +69,11 @@ var createTimeliner = function(dataset) {
     .done(function() {
       var title = dataset.get('spreadsheetTitle');
       $('.navbar .brand').text(title);
-      document.title = title + ' ' + document.title;
+      document.title = title + ' - Timeliner';
+
+      // set up twitter share button
+      // do this here rather than in page so it picks up title correctly
+      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
     });
 }
 
