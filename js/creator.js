63df3fd0001ae5c61129d98fd396940f69e3cccb
Rufus Pollock <rufus.pollock@okfn.org>
2013-02-16 19:23:41 +0000

    [#29,creator][m]: refactor to use backbone - much cleaner!.

--- js/creator.js
+++ js/creator.js
@@ -1,11 +1,10 @@
-var localStorageKey = 'timeliner-creator';
-var myList = localStorage.getItem(localStorageKey);
-myList = myList ? JSON.parse(myList) : [];
-var backupList = [];
-renderList();
-setDownloadData();
-
 jQuery(function($) {
+  var theList = new ItemList();
+  theList.fetch();
+  var listView = new ItemListView({
+    collection: theList
+  });
+
   $('.wp-search').typeahead({
     source: function(query, process) {
       queryWPTitles(query, process);
@@ -14,7 +13,7 @@ jQuery(function($) {
       $('.loading').show();
       WIKIPEDIA.getData(item, function(data) {
         $('.loading').hide();
-        addToList(data);
+        theList.addFromWP(data);
       });
 
       return item;
@@ -24,7 +23,6 @@ jQuery(function($) {
 
   $('.js-remove-all').on('click', function(e) {
     e.preventDefault();
-    backupList = JSON.parse(JSON.stringify(myList));
     myList = [];
     save();
     renderList();
@@ -49,59 +47,103 @@ function queryWPTitles(query, cb) {
   });
 }
 
-function addToList(item) {
-  delete item.raw;
-  myList.push(item);
-  save();
-  renderList();
-}
+var Item = Backbone.Model.extend({
+});
 
-function save() {
-  localStorage.setItem(localStorageKey, JSON.stringify(myList));
-  localStorage.setItem(localStorageKey + '-backup', JSON.stringify(backupList));
-  setDownloadData();
-}
+var ItemList = Backbone.Collection.extend({
+  model: Item,
+  localStorage: new Backbone.LocalStorage('TimelinerCreator'),
 
-function renderList() {
-  $('.selected').empty();
-  $.each(myList, function(idx, item) {
-    var $el = renderItem(item);
-    $('.selected').append($el);
-  });
-};
-
-function setDownloadData() {
-  var fields = [ 'Title', 'Start', 'End', 'Image', 'Place', 'Location', 'Description'];
-  var out = [];
-  out.push(fields);
-  $.each(myList, function(idx, item) {
-    var row = $.map(fields, function(field) {
-      if (field == 'Description') {
-        field = 'summary';
-      }
-      if (field == 'Location') {
-        // TODO: sort this out
-        return '';
-      } else {
-        var out = item.summary[field.toLowerCase()];
-        return out ? out : '';
-      }
-    });
-    out.push(row);
-  });
-  var csv = recline.Backend.CSV.serializeCSV(out);
-  csv = encodeURIComponent(csv);
-  $('.js-download-all').attr('href', 'data:text/csv;charset=utf8,' + csv);
-}
-
-function renderItem(info) {
-  var $el = $('.results').clone();
-  $el.show();
-
-  var summaryInfo = info.summary;
-  for (key in summaryInfo) {
-    $el.find('.summary .' + key).text(summaryInfo[key]);
+  addFromWP: function(item) {
+    delete item.raw;
+    this.create(item);
   }
-  $el.find('.summary .thumbnail').attr('src', summaryInfo.image);
-  return $el;
-};
+});
+
+var ItemListView = Backbone.View.extend({
+  initialize: function() {
+    var that = this;
+    this.collection.bind('add', function(e) {
+      that.onChange();
+    });
+    this.collection.bind('change', function(e) {
+      that.onChange();
+    });
+    that.onChange();
+  },
+
+  onChange: function() {
+    this.render();
+    this.setDownloadData();
+  },
+
+  render: function() {
+    var that = this;
+    $('.selected').empty();
+    this.collection.each(function(item) {
+      var $el = that.renderItem(item.toJSON());
+      $('.selected').append($el);
+    });
+
+    return this;
+  },
+
+  templateItem: ' \
+    <div class="results"> \
+      <div class="summary well"> \
+        <img src="" class="thumbnail" style="float: right;" /> \
+        <h4> \
+          <span class="title"></span> \
+        </h4> \
+        <p> \
+          Type: <span class="type"></span> \
+          <br /> \
+          Location: <span class="place"></span> \
+          <br /> \
+          Dates: <span class="start"></span> &mdash; <span class="end"></span> \
+        </p> \
+        <p class="summary"></p> \
+        <div style="clear: both;"></div> \
+      </div> \
+    </div> \
+  ',
+
+  renderItem: function(info) {
+    var $el = $(this.templateItem);
+
+    var summaryInfo = info.summary;
+    for (key in summaryInfo) {
+      $el.find('.summary .' + key).text(summaryInfo[key]);
+    }
+    $el.find('.summary .thumbnail').attr('src', summaryInfo.image);
+    return $el;
+  },
+
+  setDownloadData: function() {
+    var fields = [ 'Title', 'Start', 'End', 'Image', 'Place', 'Location', 'Description'];
+    var out = [];
+    out.push(fields);
+    this.collection.each(function(item) {
+      item = item.toJSON();
+      var row = $.map(fields, function(field) {
+        if (field == 'Description') {
+          field = 'summary';
+        }
+        if (field == 'Location') {
+          // TODO: sort this out
+          return '';
+        } else {
+          var out = item.summary[field.toLowerCase()];
+          return out ? out : '';
+        }
+      });
+      out.push(row);
+    });
+    var csv = recline.Backend.CSV.serializeCSV(out);
+    csv = encodeURIComponent(csv);
+    $('.js-download-all').attr('href', 'data:text/csv;charset=utf8,' + csv);
+  }
+
+});
+
+
