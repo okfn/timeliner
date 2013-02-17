6ff4182d6a657f6516c4bcdf14ff23ce9bd9af77
Rufus Pollock <rufus.pollock@okfn.org>
2013-02-17 10:54:37 +0000

    [view,refactor][s]: turn createTimeliner into a Backbone.View to make extension cleaner.

--- js/view.js
+++ js/view.js
@@ -13,7 +13,11 @@ jQuery(function($) {
     $('body').addClass('embed');
   }
   var dataset = new recline.Model.Dataset(state);
-  createTimeliner(dataset);
+  var timeliner = new TimelinerView({
+    model: dataset,
+    el: $('.data-views')
+  });
+  timeliner.render();
 
   $('.js-embed').on('click', function(e) {
     e.preventDefault();
@@ -24,67 +28,67 @@ jQuery(function($) {
   });
 });
 
-var createTimeliner = function(dataset) {
-  var self = this;
-  var $el = $('.data-views .timeline');
-  // explicitly set width as otherwise Timeline does extends a bit too far (seems to use window width rather than width of actual div)
-  // $el.width((this.el.width() - 45)/2.0);
-  var timeline = new recline.View.Timeline({
-    model: dataset,
-    el: $el
-  });
-  timeline.render();
-  timeline.convertRecord = function(record, fields) {
-    if (record.attributes.start[0] == "'") {
-      record.attributes.start = record.attributes.start.slice(1);
-    }
-    if (record.attributes.end[0] == "'") {
-      record.attributes.end = record.attributes.end.slice(1);
-    }
-    try {
-      var out = this._convertRecord(record, fields);
-    } catch (e) {
-      out = null;
-    }
-    if (!out) {
-      alert('Failed to extract date from: ' + JSON.stringify(record.toJSON()));
-      return null;
-    }
-    if (record.get('image')) {
-      out.asset = {
-        media: record.get('image')
-      };
-    }
-    out.text = record.get('description');
-    if (record.get('source')) {
-      var s = record.get('source');
-      if (record.get('sourceurl')) {
-        s = '<a href="' + record.get('sourceurl') + '">' + s + '</a>';
-      }
-      out.text += '<p class="source">Source: ' + s + '</p>';
-    }
-    // hacky but it will work ...
-    // do not want time part of the dates
-    out.startDate = String(out.startDate.getFullYear()) + ',' + String(out.startDate.getMonth()+1) + ',' + String(out.startDate.getDate());
-    return out;
-  }
-
-  this.map = new recline.View.Map({
-    model: dataset
-  });
-  $('.data-views .map').append(this.map.el);
-  this.map.render();
-
-  // load the data
-  dataset.fetch()
-    .done(function() {
-      var title = dataset.get('spreadsheetTitle');
-      $('.navbar .brand').text(title);
-      document.title = title + ' - Timeliner';
-
-      // set up twitter share button
-      // do this here rather than in page so it picks up title correctly
-      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+var TimelinerView = Backbone.View.extend({
+  render: function() {
+    var self = this;
+    // explicitly set width as otherwise Timeline does extends a bit too far (seems to use window width rather than width of actual div)
+    // $el.width((this.el.width() - 45)/2.0);
+    this.timeline = new recline.View.Timeline({
+      model: this.model,
+      el: this.$el.find('.timeline')
     });
-}
+    this.timeline.convertRecord = function(record, fields) {
+      if (record.attributes.start[0] == "'") {
+        record.attributes.start = record.attributes.start.slice(1);
+      }
+      if (record.attributes.end[0] == "'") {
+        record.attributes.end = record.attributes.end.slice(1);
+      }
+      try {
+        var out = this._convertRecord(record, fields);
+      } catch (e) {
+        out = null;
+      }
+      if (!out) {
+        alert('Failed to extract date from: ' + JSON.stringify(record.toJSON()));
+        return null;
+      }
+      if (record.get('image')) {
+        out.asset = {
+          media: record.get('image')
+        };
+      }
+      out.text = record.get('description');
+      if (record.get('source')) {
+        var s = record.get('source');
+        if (record.get('sourceurl')) {
+          s = '<a href="' + record.get('sourceurl') + '">' + s + '</a>';
+        }
+        out.text += '<p class="source">Source: ' + s + '</p>';
+      }
+      // hacky but it will work ...
+      // do not want time part of the dates
+      out.startDate = String(out.startDate.getFullYear()) + ',' + String(out.startDate.getMonth()+1) + ',' + String(out.startDate.getDate());
+      return out;
+    }
 
+    this.timeline.render();
+    this.map = new recline.View.Map({
+      model: this.model
+    });
+    this.$el.find('.map').append(this.map.el);
+    this.map.render();
+
+    // load the data
+    this.model.fetch()
+      .done(function() {
+        var title = self.model.get('spreadsheetTitle');
+        $('.navbar .brand').text(title);
+        document.title = title + ' - Timeliner';
+
+        // set up twitter share button
+        // do this here rather than in page so it picks up title correctly
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+      });
+  }
+});
