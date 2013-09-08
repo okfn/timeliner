okfn/timeliner

commit dc330504de436716323d453e7fc17ee40dcdc1dd

[#52,bugfix][s]: fix handling of BC (negative) dates via upgrade of recline - fixes #52.

js/view.js
@@ -103,9 +103,6 @@ var TimelinerView = Backbone.View.extend({
         }
         out.text += '<p class="source">Source: ' + s + '</p>';
       }
-      // hacky but it will work ...
-      // do not want time part of the dates
-      out.startDate = String(out.startDate.getFullYear()) + ',' + String(out.startDate.getMonth()+1) + ',' + String(out.startDate.getDate());
       return out;
     };
 

view/index.html
@@ -20,7 +20,7 @@
 
   <link rel="stylesheet" href="../vendor/leaflet.label/leaflet.label.css" />
 
-  <link rel="stylesheet" href="../vendor/recline/vendor/timeline/20120520/css/timeline.css">
+  <link rel="stylesheet" href="../vendor/recline/vendor/timeline/2.24/css/timeline.css">
 
   <!-- Recline CSS components -->
   <link rel="stylesheet" href="../vendor/recline/dist/recline.css">
@@ -100,7 +100,7 @@ Created with the <a href="http://timeliner.okfnlabs.org/"><strong>Timeliner </st
   <script type="text/javascript" src="../vendor/recline/vendor/bootstrap/2.0.2/bootstrap.js"></script>
   <script type="text/javascript" src="../vendor/recline/vendor/leaflet/0.4.4/leaflet.js"></script>
   <script type="text/javascript" src="../vendor/recline/vendor/leaflet.markercluster/leaflet.markercluster.js"></script>
-  <script type="text/javascript" src="../vendor/recline/vendor/timeline/20120520/js/timeline.js"></script>
+  <script type="text/javascript" src="../vendor/recline/vendor/timeline/2.24/js/timeline.js"></script>
   <script type="text/javascript" src="../vendor/leaflet.label/leaflet.label.js"></script>
 
   <script type="text/javascript" src="../vendor/recline/dist/recline.js"></script>

commit a08039829ceba065a090c3a440f003ce7a53b5e9

[bugfix][s]: fix broken syncing of map and timeline caused by changes in dc330504de436716323d453e7fc17ee40dcdc1dd - fixes #55.
* need recline model to sort records in time order as the timeline does and this had got broken with recent changes in recline timeline

js/view.js
@@ -66,10 +66,12 @@ var TimelinerView = Backbone.View.extend({
       }
     });
 
-    // Timeline will sort the entries by timestamp, but we need the order to be the same for the map
+    // Timeline will sort the entries by timestamp, and we need the order to be
+    // the same for the map which runs off the model
     this.model.records.comparator = function (a, b) {
-      var a = self.timeline._parseDate(a.get("start"));
-      var b = self.timeline._parseDate(b.get("start"));
+      // VMM.Date.parse is the timelinejs date parser
+      var a = VMM.Date.parse(self.timeline._parseDate(a.get("start")));
+      var b = VMM.Date.parse(self.timeline._parseDate(b.get("start")));
       return a - b;
     };
 